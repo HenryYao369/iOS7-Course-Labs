cd584549b0ee7f818998f24f581343df58641226
HenryYAO <hengzhi369@gmail.com>
2015-06-07 18:19:17 +0800

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Hengzhi Yao on 6/7/15.
+//  Copyright (c) 2015 Hengzhi Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Hengzhi Yao on 6/7/15.
+//  Copyright (c) 2015 Hengzhi Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
